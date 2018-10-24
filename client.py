5796c91b980675f5fafede75297cf25b247b460f
jtaeno <juliotaeno@yahoo.es>
2018-10-24 11:51:57 +0200

    cambio de client.py, punto 4 de la practica

--- client.py
+++ client.py
@@ -5,17 +5,21 @@ Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
+SERVER =  sys.argv[1]
 PORT = 6001
-LINE = '¡Hola mundo!'
+LINE =  sys.argv[3:]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+frase = ''
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    for pal in LINE:
+        frase += pal + " " 
+    print("Enviando:", frase)
+    my_socket.send(bytes(frase, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
